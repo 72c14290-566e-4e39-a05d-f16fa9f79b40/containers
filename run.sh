c3a6c1cf7bf099119661626cebc04c251f684359
user9 <user9@localhost>
2018-07-02 22:59:16 -0400

    make it run

--- run.sh
+++ run.sh
@@ -2,9 +2,15 @@
 
 pushd Ubuntu
   ansible-container build 
+  ansible-container run
+  curl localhost:80
+  ansible-container stop
 popd
 pushd Centos7
   ansible-container build 
+  ansible-container run
+  curl localhost:80
+  ansible-container stop
 popd
   
 
